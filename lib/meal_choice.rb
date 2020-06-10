cc3a06634f1a5f3bf02ea5c199e2d934364362e3
Hailey Ringier <haileyringier@outlook.com>
2020-06-10 23:47:16 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 def meal_choice (veg1, veg2, protein = "tofu")
   welcome_note = "What a nutritious meal!"
   nutrious_meal = "A plate of #{protein} with #{veg1} and #{veg2}."
-  welcome_note 
-  nutrious_meal
+  puts welcome_note 
+  puts nutrious_meal
 end
